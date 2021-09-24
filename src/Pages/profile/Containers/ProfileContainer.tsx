7f0b158d9120784d032449205badd0bab3086a0d
palmmy2542 <palm_my2542@hotmail.com>
2021-09-24 22:49:19 +0700

    Create UserAvatar

--- src/Pages/profile/Containers/ProfileContainer.tsx
+++ src/Pages/profile/Containers/ProfileContainer.tsx
@@ -1,25 +1,13 @@
-import { Avatar } from "antd";
 import React from "react";
-import { useParams } from "react-router";
 import { UserOutlined } from "@ant-design/icons";
-import "../index.css";
+import UserAvatar from "../Components/UserAvatar";
 
 const ProfileContainer = (props: any) => {
-  const { username } = useParams<{ username: string }>();
   return (
     <div id="profile">
-      <Avatar
-        size={{
-          xs: 24,
-          sm: 32,
-          md: 40,
-          lg: 64,
-          xl: 80,
-          xxl: 100,
-        }}
-        icon={<UserOutlined />}
-      />
-      <h1>{username}</h1>
+      <UserAvatar>
+        <UserOutlined />
+      </UserAvatar>
     </div>
   );
 };
