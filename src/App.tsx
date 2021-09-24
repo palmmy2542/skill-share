40df6187010bb8732ad883847ab09f0ff388edd4
palmmy2542 <palm_my2542@hotmail.com>
2021-09-24 23:29:43 +0700

    Apply Provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,23 @@ import Home from "./Pages/home/index";
 import Profile from "./Pages/profile/index";
 
 import "./App.css";
+import { UserDataProvider } from "./Domains/UserData/useUserDataContext";
 
 function App() {
   return (
     <div className="App">
-      <div className="layout">
-        <Switch>
-          <Route exact path="/:username">
-            <Profile />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <UserDataProvider>
+        <div className="layout">
+          <Switch>
+            <Route exact path="/:username">
+              <Profile />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </UserDataProvider>
     </div>
   );
 }
