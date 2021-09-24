ab918242699531c92e6cb9375438f29f8c1f4d73
palmmy2542 <palm_my2542@hotmail.com>
2021-09-24 21:57:14 +0700

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import Home from "./Pages/home/index";
+import { Layout } from "antd";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <div className="layout">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
