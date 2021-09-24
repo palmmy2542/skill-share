eeb12cbe76d14f20561ce424e445731d39c2c591
palmmy2542 <palm_my2542@hotmail.com>
2021-09-24 21:59:37 +0700

    Remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import Home from "./Pages/home/index";
-import { Layout } from "antd";
 import "./App.css";
 
 function App() {
