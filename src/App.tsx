9ae9f2444351cdaf8f4fd412c45e94c4de66cf1f
palmmy2542 <palm_my2542@hotmail.com>
2021-09-24 21:46:18 +0700

    Initial Profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import Home from "./Pages/home/index";
+import Profile from "./Pages/profile/index";
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route exact path="/profile/:id">
+          <Profile />
+        </Route>
         <Route exact path="/">
           <Home />
         </Route>
---
+++ src/Pages/profile/Containers/ProfileContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileContainer = (props: any) => {
+  return (
+    <div id="home">
+      <h1>Home page</h1>
+    </div>
+  );
+};
+
+export default ProfileContainer;
