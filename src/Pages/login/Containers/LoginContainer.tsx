e8c5a7eac4a4d0aa3db187caaaf79c523994009e
noof3310 <noof3310@gmail.com>
2021-09-26 18:36:27 +0700

    Edit Login Page

--- src/Pages/login/Containers/LoginContainer.tsx
+++ src/Pages/login/Containers/LoginContainer.tsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { Button, Divider } from "antd";
 import "../index.css";
+import LoginForm from "../Components/LoginForm";
+import { Layout } from "antd";
+const { Content, Footer } = Layout;
 
 const LoginContainer = (props: any) => {
   return (
-    <div id="login">
-      <h1>Login page</h1>
-    </div>
+    <Layout id="layout">
+      <Content>
+        <div id="login">
+          <h1>Log in</h1>
+          <LoginForm />
+        </div>
+      </Content>
+      <Footer>
+        Don't have an account? <a href="">Sign up!</a>
+      </Footer>
+    </Layout>
   );
 };
 
