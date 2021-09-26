8a6623fea9d52f892f8cf30915fba39b34bc3f88
noof3310 <noof3310@gmail.com>
2021-09-26 19:04:11 +0700

    Update LoginContainer

--- src/Pages/login/Containers/LoginContainer.tsx
+++ src/Pages/login/Containers/LoginContainer.tsx
@@ -1,21 +1,17 @@
 import "../index.css";
 import LoginForm from "../Components/LoginForm";
-import { Layout } from "antd";
-const { Content, Footer } = Layout;
+import { Footer } from "antd/lib/layout/layout";
+
 
 const LoginContainer = (props: any) => {
   return (
-    <Layout id="layout">
-      <Content>
-        <div id="login">
-          <h1>Log in</h1>
-          <LoginForm />
-        </div>
-      </Content>
+    <div id="login">
+      <h1>Log in</h1>
+      <LoginForm />
       <Footer>
         Don't have an account? <a href="">Sign up!</a>
       </Footer>
-    </Layout>
+    </div>
   );
 };
 
