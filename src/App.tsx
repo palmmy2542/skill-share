275e61b031b25c3519d28fb3ab6d7f8089cd66d0
palmmy2542 <palm_my2542@hotmail.com>
2021-10-17 00:07:54 +0700

    Handle path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./Pages/home/index";
 import Profile from "./Pages/profile/index";
 import Register from "./Pages/register/index";
@@ -8,35 +8,34 @@ import Feed from "./Pages/feed";
 
 import "./App.css";
 import { UserDataProvider } from "./Domains/UserData/useUserDataContext";
-import { UserAuthenticationProvider } from "./Domains/UserAuthentication/useUserAuthentication";
 import { ClipFeedProvider } from "./Domains/ClipFeed/useClipFeed";
+import useUserAuthenticationContext from "./Domains/UserAuthentication/useUserAuthentication";
+import ClipFeed from "./Pages/feed/Components/ClipFeed";
 
 function App() {
+  const { canAccessService } = useUserAuthenticationContext();
+  const username = localStorage.getItem("skillUsername");
+
   return (
     <div className="App">
-      <UserAuthenticationProvider>
-        <UserDataProvider>
-          <ClipFeedProvider>
-            <Switch>
-              <Route exact path="/register">
-                <Register />
-              </Route>
-              <Route exact path="/login">
-                <Login />
-              </Route>
-              <Route exact path="/:userParam/:videoId">
-                <Feed />
-              </Route>
-              <Route exact path="/:userParam">
-                <Profile />
-              </Route>
-              <Route exact path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </ClipFeedProvider>
-        </UserDataProvider>
-      </UserAuthenticationProvider>
+      <UserDataProvider>
+        <ClipFeedProvider>
+          <Switch>
+            <Route exact path="/">
+              <ClipFeed />
+            </Route>
+            <Route exact path="/register">
+              <Register />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/:userParam">
+              <Profile />
+            </Route>
+          </Switch>
+        </ClipFeedProvider>
+      </UserDataProvider>
     </div>
   );
 }
