6b4911a07778c72ca1bbcfd11c9dd1126f50b5da
palmmy2542 <palm_my2542@hotmail.com>
2021-10-18 10:02:32 +0700

    And Upload component

--- src/Components/BottomNav/BottomNav.tsx
+++ src/Components/BottomNav/BottomNav.tsx
@@ -1,21 +1,60 @@
-import React from "react";
-import { PlayCircleOutlined, PlusCircleTwoTone, UserOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
+import {
+  PlayCircleOutlined,
+  PlusCircleTwoTone,
+  UserOutlined,
+} from "@ant-design/icons";
 import "./index.css";
-import { Divider } from "antd";
+import { Button, Divider, Drawer } from "antd";
+import UploadForm from "../../Pages/upload/Components/UploadForm";
+import { useHistory } from "react-router";
 
 const BottomNav = ({ username }: { username: string }) => {
+  const [visible, setVisible] = useState(false);
+  const history = useHistory();
+
+  const handleClose = () => {
+    setVisible(false);
+  };
+
+  const handleOpen = () => {
+    setVisible(true);
+  };
+
+  const handlePush = (path: string) => {
+    history.push(path);
+  };
   return (
     <div className="bottom-nav">
-      <a className="bottom-nav-btn" href="/learn">
-        <PlayCircleOutlined className="icon" />
-      </a>
-      <a className="bottom-nav-btn" href="/upload">
-        <PlusCircleTwoTone className="icon-plus"/>
-        <p></p>
-      </a>
-      <a className="bottom-nav-btn" href={`/${username}`}>
-        <UserOutlined className="icon" />
-      </a>
+      <PlayCircleOutlined
+        className="icon bottom-nav-btn"
+        style={{ fontSize: "30px" }}
+        onClick={() => handlePush("/learn")}
+      />
+      {/* <a className="bottom-nav-btn" href="/upload">
+        <PlusCircleTwoTone className="icon-plus" />
+
+      </a> */}
+
+      <PlusCircleTwoTone className="icon-plus" onClick={handleOpen} />
+
+      <Drawer
+        placement={"bottom"}
+        visible={visible}
+        onClose={handleClose}
+        closable={true}
+        width={"100%"}
+        keyboard
+        destroyOnClose
+        height="100%"
+      >
+        <UploadForm />
+      </Drawer>
+      <UserOutlined
+        className="icon bottom-nav-btn"
+        style={{ fontSize: "30px" }}
+        onClick={() => handlePush(`/${username}`)}
+      />
     </div>
   );
 };
