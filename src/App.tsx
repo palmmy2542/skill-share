4e0ca97dd8de1ac9fe0ac54991be92512f5decca
noof3310 <noof3310@gmail.com>
2021-11-09 23:27:51 +0700

    ADD TEST ROUTE IN HOME PAGE FOR DEMO PURPOSE

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Profile from "./Pages/profile/index";
 import Register from "./Pages/register/index";
 import Login from "./Pages/login/index";
 import Learn from "./Pages/learn/index";
+import Home from "./Pages/home/index";
 
 import "./App.css";
 import { ClipFeedProvider } from "./Domains/ClipFeed/useClipFeed";
@@ -34,6 +35,9 @@ function App() {
               ></Route>
               <Route exact path="/learn">
                 <Learn />
+              </Route>
+              <Route exact path="/test">
+                <Home />
               </Route>
               <Route exact path="/register">
                 <Register />
--- src/Pages/home/Containers/HomeContainer.tsx
+++ src/Pages/home/Containers/HomeContainer.tsx
@@ -1,9 +1,66 @@
 import React from "react";
+import { useState } from "react";
+import BottomMenu from "../../../Components/Clip/BottomMenu";
+import EditClip from "../../../Components/Clip/EditClip";
+import PlaylistFeed from "../../../Components/PlaylistFeed/PlaylistFeed";
+import usePlaylistContext from "../../../Domains/Playlist/usePlaylist";
 
 const HomeContainer = (props: any) => {
+
+  const title = "Clip 1";
+  const description = "This clip is for testing";
+  const tags = ["clip", "testing", "se3"];
+
+  const [isShow, setIsShow] = useState(true);
+  const [isShowEditClip, setIsShowEditClip] = useState(false);
+  const [isShowPlaylist, setIsShowPlaylist] = useState(false);
+  const { playlist } = usePlaylistContext();
+  
+  const handleCloseBottomMenu = () => {
+    setIsShow(false);
+  };
+
+  const handleOpenBottomMenu = () => {
+    setIsShow(true);
+  };
+
+  const handleCloseEditClip = () => {
+    setIsShowEditClip(false);
+  };
+
+  const handleOpenEditClip = () => {
+    setIsShowEditClip(true);
+  };
+
+  const handleClosePlayList = () => {
+    setIsShowPlaylist(false);
+  };
+
+  const handleOpenPlayList = () => {
+    setIsShowPlaylist(true);
+  };
+
   return (
     <div id="home">
       <h1>Home page</h1>
+      <EditClip
+        visible={isShowEditClip}
+        title={title}
+        description={description}
+        tags={tags}
+        handleClose={handleCloseEditClip}
+      />
+      <PlaylistFeed
+        visible={isShowPlaylist}
+        playlist={playlist}
+        handleClose={handleClosePlayList}
+      />
+      <BottomMenu
+        visible={isShow}
+        handleClose={handleCloseBottomMenu}
+        handleOpenEditClip={handleOpenEditClip}
+        handleOpenPlayList={handleOpenPlayList}
+      />
     </div>
   );
 };
