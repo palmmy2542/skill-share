53ebf4b5a9c8628edcb7ba938e3d517a84657d5f
palmmy2542 <palm_my2542@hotmail.com>
2021-11-12 12:28:27 +0700

    Move Playlist API

--- src/Components/PlaylistFeed/utils.ts
+++ src/Components/PlaylistFeed/utils.ts
@@ -1,1 +1,111 @@
-export const STATE = { EDIT: "EDIT", SAVE: "SAVE" };
+import { message } from "antd";
+import axios from "axios";
+import { PLAYLIST_HOST } from "../../const";
+
+const postNewPlaylist = async ({
+  token,
+  title,
+  description,
+  videoList,
+  permission,
+  userId,
+}: {
+  token: string | undefined;
+  title: string;
+  description: string;
+  videoList: string;
+  permission: string;
+  userId: string;
+}): Promise<any> => {
+  if (token) {
+    const config = {
+      headers: {
+        Authorization: `${token.trim()}`,
+      },
+    };
+    return axios
+      .post(
+        `${PLAYLIST_HOST}/playlists/playlist`,
+        { title, description, videoList, permission, userId },
+        config
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          return response.data;
+        }
+      })
+      .catch((err) => message.error(err.response.data.message));
+  }
+  return null;
+};
+
+const editPlaylist = async ({
+  token,
+  id,
+  title,
+  description,
+  videoList,
+  permission,
+  userId,
+}: {
+  token: string | undefined;
+  id: string;
+  title: string;
+  description: string;
+  videoList: string;
+  permission: string;
+  userId: string;
+}): Promise<any> => {
+  if (token) {
+    const config = {
+      headers: {
+        Authorization: `${token.trim()}`,
+      },
+    };
+    return axios
+      .post(
+        `${PLAYLIST_HOST}/playlists/edit`,
+        { id, title, description, videoList, permission, userId },
+        config
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          return response.data;
+        }
+      })
+      .catch((err) => message.error(err.response.data.message));
+  }
+  return null;
+};
+
+const deletePlaylist = async ({
+  token,
+  id,
+  userId,
+}: {
+  token: string | undefined;
+  id: string;
+  userId: string;
+}): Promise<any> => {
+  if (token) {
+    const config = {
+      headers: {
+        Authorization: `${token.trim()}`,
+      },
+    };
+    return axios
+      .delete(
+        `${PLAYLIST_HOST}/playlists/delete?id=${id}&?userId=${userId}`,
+        config
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          return response.data;
+        }
+      })
+      .catch((err) => message.error(err.response.data.message));
+  }
+  return null;
+};
+
+export { postNewPlaylist, deletePlaylist, editPlaylist };
--- src/Domains/Playlist/usePlaylist.tsx
+++ src/Domains/Playlist/usePlaylist.tsx
@@ -190,113 +190,6 @@ const usePlaylist = () => {
     }
     return null;
   };
-
-  const postNewPlaylist = async ({
-    token,
-    title,
-    description,
-    videoList,
-    permission,
-    userId,
-  }: {
-    token: string | undefined;
-    title: string;
-    description: string;
-    videoList: string;
-    permission: string;
-    userId: string;
-  }): Promise<any> => {
-    if (token) {
-      const config = {
-        headers: {
-          Authorization: `${token.trim()}`,
-        },
-      };
-      return axios
-        .post(
-          `${PLAYLIST_HOST}/playlists/playlist`,
-          { title, description, videoList, permission, userId },
-          config
-        )
-        .then((response) => {
-          if (response.status === 200) {
-            return response.data;
-          }
-        })
-        .catch((err) => message.error(err.response.data.message));
-    }
-    return null;
-  };
-
-  const editPlaylist = async ({
-    token,
-    id,
-    title,
-    description,
-    videoList,
-    permission,
-    userId,
-  }: {
-    token: string | undefined;
-    id: string;
-    title: string;
-    description: string;
-    videoList: string;
-    permission: string;
-    userId: string;
-  }): Promise<any> => {
-    if (token) {
-      const config = {
-        headers: {
-          Authorization: `${token.trim()}`,
-        },
-      };
-      return axios
-        .post(
-          `${PLAYLIST_HOST}/playlists/edit`,
-          { id, title, description, videoList, permission, userId },
-          config
-        )
-        .then((response) => {
-          if (response.status === 200) {
-            return response.data;
-          }
-        })
-        .catch((err) => message.error(err.response.data.message));
-    }
-    return null;
-  };
-
-  const deletePlaylist = async ({
-    token,
-    id,
-    userId,
-  }: {
-    token: string | undefined;
-    id: string;
-    userId: string;
-  }): Promise<any> => {
-    if (token) {
-      const config = {
-        headers: {
-          Authorization: `${token.trim()}`,
-        },
-      };
-      return axios
-        .delete(
-          `${PLAYLIST_HOST}/playlists/delete?id=${id}&?userId=${userId}`,
-          config
-        )
-        .then((response) => {
-          if (response.status === 200) {
-            return response.data;
-          }
-        })
-        .catch((err) => message.error(err.response.data.message));
-    }
-    return null;
-  };
-
   return {
     playlist,
     videoInPlaylist,
@@ -304,9 +197,6 @@ const usePlaylist = () => {
     setPlaylist,
     getPlaylistByUserId,
     getAllPlaylist,
-    postNewPlaylist,
-    editPlaylist,
-    deletePlaylist,
   };
 };
 
